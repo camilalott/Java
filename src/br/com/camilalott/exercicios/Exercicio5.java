365c6ac52f7606ce2827308b94a78e4faf83b1b3
camilaclott <camilaclott@gmail.com>
2015-02-24 00:32:31 -0300

    Exercicio 5. Converter Celsius para Fahrenheit e vice versa.

--- src/br/com/camilalott/exercicios/Exercicio5.java
+++ src/br/com/camilalott/exercicios/Exercicio5.java
@@ -1,13 +1,13 @@
 package br.com.camilalott.exercicios;
+
 import javax.swing.JOptionPane;
 
 /**
- * 6. Ler o número de horas trabalhadas e o valor da hora do professor e
- * imprimir o salário bruto e líquido, sabendo que o desconto do IR segue a
- * fórmula: Salário menor ou igual a R$ 1200,00 – desconto ZERO Salário maior
- * que R$1200,00 e menor ou igual a R$ 1800,00 – desconto de 10% Salário maior
- * que R$1800,00 e menor ou igual a R$ 2200,00 – desconto de 15% Salário maior
- * que R$2200,00 – desconto de 27,5%
+ * 5. Ler uma temperatura e a escala (C-Celsius ou F- Fahrenheit ) e convertê-la
+ * para a outra escala. Ou seja, se o usuário informar a temperatura em Celsius
+ * esta deverá convertida para Fahrenheit e vice-versa. Fórmulas:
+ * F = 32 + ((212 − 32) / 100) x C 
+ * C = (F-32) x (100/(212-32))
  * 
  * @author Camila
  *
@@ -15,28 +15,47 @@ import javax.swing.JOptionPane;
 public class Exercicio5 {
 	public static void main(String[] args) {
 
-		// Ler o numero de horas e valor da hora
-		int numeroHoras = Integer.parseInt(JOptionPane
-				.showInputDialog("Informe o numero de horas trabalhadas:"));
-		double valorHora = Double.parseDouble(JOptionPane
-				.showInputDialog("Informe o valor da hora trabalhada:"));
+		// Ler a temperatura e qual a escala
+		String tipoEscala = JOptionPane
+				.showInputDialog("Escolhe o tipo da temperatura:\nF - De Fahrenheit para Celsius\nC - De Celcius para Fahreinheit");
 
-		// Calcular Salario Bruto
-		double salarioBruto = numeroHoras * valorHora;
-
-		// Calcular salario Liquido, desconto de Imposto de renda
-		double salarioLiquido = 0;
-		if ((salarioBruto > 1200) && (salarioBruto <= 1800)) {
-			salarioLiquido = salarioBruto - (salarioBruto * 0.10);
-		}
-		if ((salarioBruto > 1800) && (salarioBruto <= 2200)) {
-			salarioLiquido = salarioBruto - (salarioBruto * 0.15);
-		}
-		if (salarioBruto > 2200) {
-			salarioLiquido = salarioBruto - (salarioBruto * 0.275);
+		if (!"c".equalsIgnoreCase(tipoEscala)
+				&& !"f".equalsIgnoreCase(tipoEscala)) {
+			System.out
+					.println("Essa temperatura informada é invalida. Informe C ou F.");
+			System.exit(1);
 		}
 
-		System.out.println("O salario bruto é: " + salarioBruto
-				+ "\nEo salario liquido é: " + salarioLiquido);
+		double temperatura = Double.parseDouble(JOptionPane
+				.showInputDialog("Informe os graus da temperatura:"));
+		double temperaturaConvertida = 0;
+
+		// sempre coloque a constante. equals porque se ele nao digita nada vai
+		// dar erro.
+		if ("c".equalsIgnoreCase(tipoEscala)) {
+			if (temperatura >= -273.15) {
+				temperaturaConvertida = 32 + ((212 - 32) / 100.0) * temperatura;
+			} else {
+				System.out
+						.println("Nao existe temperatura menor do que -273,15° Celsius");
+				System.exit(1);
+			}
+		} else if ("f".equalsIgnoreCase(tipoEscala)) {
+			if (temperatura >= 0) {
+				temperaturaConvertida = (temperatura - 32.0) * (100.0 / (212.0 - 32.0));
+			} else {
+				System.out
+						.println("Nao existe temperatura menor do que 0° Fahrenheit");
+				System.exit(1);
+			}
+		}
+
+		System.out.println("A temperatura convertida é: "
+				+ temperaturaConvertida);
+
 	}
+
 }
+
+// Adicionar as seguintes condicoes, nao existe Fahrenheite menor do que 0
+// E Celsius menor que -273,15
