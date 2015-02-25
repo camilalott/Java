ea78f3eef45f2ca203f3794c6e205a16c4c5983a
camilaclott <camilaclott@gmail.com>
2015-02-25 17:27:21 -0300

    3. Calcula intervalo de dois horarios. Saida em hora, minuto e segundo.

--- src/br/com/camilalott/exercicios/Exercicio3.java
+++ src/br/com/camilalott/exercicios/Exercicio3.java
@@ -4,9 +4,9 @@ import javax.swing.JOptionPane;
 
 /**
  * 3. Ler dois valores de hora, minuto e segundo (h1, m1, s1, h2, m2, s2). Em
- * seguida imprimir o intervalo entre esses dois horários em hora, minuto e
- * segundo. Dica: converta os horários lidos para segundos, calcule a diferença
- * e converta a diferença em hora, minuto e segundo.
+ * seguida imprimir o intervalo entre esses dois horarios em hora, minuto e
+ * segundo. Dica: converta os horarios lidos para segundos, calcule a diferenca
+ * e converta a diferenca em hora, minuto e segundo.
  * 
  * @author Camila
  *
